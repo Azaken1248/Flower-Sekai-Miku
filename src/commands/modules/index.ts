af52ab415d4ee0e4d26dae24300555561bef9a06
Azaken1248 <suvkrm@gmail.com>
2026-04-25 10:34:49 +0530

    update command builder to include our new commands

--- src/commands/modules/index.ts
+++ src/commands/modules/index.ts
@@ -1,10 +1,12 @@
 import type { AppConfig } from "../../config/env";
+import { DeboardCommand } from "./crew/deboard.command";
 import type { SlashCommand } from "../contracts/slash-command";
 import { OnboardCommand } from "./crew/onboard.command";
 import { AssignCommand } from "./tasks/assign.command";
 import { ExtensionCommand } from "./tasks/extension.command";
 import { HelloCommand } from "./utility/hello.command";
 import { PingCommand } from "./utility/ping.command";
+import { ProfileCommand } from "./utility/profile.command";
 import { UptimeCommand } from "./utility/uptime.command";
 
 export const buildCommandModules = (config: AppConfig): SlashCommand[] => {
@@ -12,8 +14,10 @@ export const buildCommandModules = (config: AppConfig): SlashCommand[] => {
 
   return [
     new OnboardCommand(),
+    new DeboardCommand(),
     new AssignCommand(adminRoleIds, config.roles.specialized),
     new ExtensionCommand(),
+    new ProfileCommand(),
     new PingCommand(),
     new UptimeCommand(),
     new HelloCommand(),
