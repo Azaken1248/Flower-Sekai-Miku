6826b4db3b6b7c094c394b2a2da023292088c8c4
Azaken1248 <suvkrm@gmail.com>
2026-05-08 01:45:24 +0530

    add strike service to command context

--- src/commands/contracts/command-execution-context.ts
+++ src/commands/contracts/command-execution-context.ts
@@ -2,6 +2,7 @@ import type { AppConfig } from "../../config/env";
 import type { Logger } from "../../core/logger/logger";
 import type { AssignmentService } from "../../services/assignment-service";
 import type { ConfigCacheService } from "../../services/config-cache-service";
+import type { StrikeService } from "../../services/strike-service";
 import type { UserService } from "../../services/user-service";
 
 export interface CommandExecutionContext {
@@ -9,5 +10,6 @@ export interface CommandExecutionContext {
   logger: Logger;
   userService: UserService;
   assignmentService: AssignmentService;
+  strikeService: StrikeService;
   configCacheService: ConfigCacheService;
 }
